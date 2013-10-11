ee8f6aa5bae2bceb94a0b3e9463013d82e676f23
NachoVeiga <veiganacho@gmail.com>
2013-10-11 20:57:48 +0200

    Estilizando el código
    Agregando y sacando espacios y tabs

--- Dedicated Workers/crono.js
+++ Dedicated Workers/crono.js
@@ -1,13 +1,12 @@
 var i=0;
 
-function cronometro()
-{
-self.postMessage(i);
-i++;
-setTimeout("cronometro()",1000);
+function cronometro(){
+	self.postMessage(i);
+	i++;
+	setTimeout("cronometro()",1000);
 
-// Deteniendo el worker
-// if (i>6){self.close();}
+	//Deteniendo el worker
+    if (i>4){self.close();}
 }
 
 cronometro();
